b923c68b7eb1cf4dfb02a33c56a9ff0d5ea5cb91
tigerros <glacific@gmail.com>
2021-04-22 12:45:08 +0200

    test2

--- js/themes.js
+++ js/themes.js
@@ -1,11 +1,11 @@
 var theme = document.getElementById('theme'); // TODO: Toggle theme
 
 function darkTheme() {
-  if (theme.href === '../css/light.css') {
-    theme.href = '../css/dark.css';
+  if (theme.href === 'css/light.css') {
+    theme.href = 'css/dark.css';
     console.log(theme.href);
   } else {
-    theme.href = '../css/light.css';
+    theme.href = 'css/light.css';
     console.log(theme.href);
   }
 }
