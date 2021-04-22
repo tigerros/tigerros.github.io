68c67e68cc2b2fd3067daac8691fa9b88899b13b
tigerros <glacific@gmail.com>
2021-04-22 12:40:10 +0200

    Test

--- js/pow.js
+++ js/pow.js
@@ -1,13 +1,13 @@
 function powerFunc() { // Multiplies the "base" number to the power of the "exponent"
-  var base = document.getElementById('base').value
-  var exponent = document.getElementById('exponent').value
+  var base = document.getElementById('base').value;
+  var exponent = document.getElementById('exponent').value;
 
-  return Math.pow(base, exponent)
+  return Math.pow(base, exponent);
 }
 
 function resultFunc() { // Gets result
-  var result = document.getElementById('result')
+  var result = document.getElementById('result');
 
-  result.innerHTML = powerFunc()
-  console.log("Showed result")
+  result.innerHTML = powerFunc();
+  console.log("Showed result");
 }
--- js/themes.js
+++ js/themes.js
@@ -1,9 +1,15 @@
-var theme = document.getElementById('theme') // TODO: Toggle theme
+var theme = document.getElementById('theme'); // TODO: Toggle theme
 
-function darkTheme(path){
-  theme.href = path
+function darkTheme(){
+  if (theme.href === '../css/light.css') {
+    theme.href = '../css/dark.css';
+    console.log(theme.href);
+  } else {
+    theme.href = '../css/light.css';
+    console.log(theme.href);
+  }
 }
 
-function lightTheme(path){
-  theme.href = path
-}
+//function lightTheme(path){
+  //theme.href = path;
+//}
