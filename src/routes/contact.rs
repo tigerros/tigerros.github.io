aaa531cd22b78cb75259f897a4e1d3ad4ce91769
Leonard <tigerros.gh@gmail.com>
2023-08-14 18:07:54 +0200

    Add fallback file and add `dist` to .gitignore
    Also fix LinkedIn URL in contacts.

--- src/routes/contact.rs
+++ src/routes/contact.rs
@@ -6,7 +6,7 @@ pub fn contact() -> Html {
         <ul>
             <li><a href="https://github.com/tigerros">{"GitHub"}</a></li>
             <li><a href="mailto:tigerros.gh@gmail.com">{"Email"}</a></li>
-            <li><a href="www.linkedin.com/in/aurel-leonard-danel-195470232">{"LinkedIn"}</a></li>
+            <li><a href="https://www.linkedin.com/in/aurel-leonard-danel-195470232">{"LinkedIn"}</a></li>
         </ul>
     }
 }
